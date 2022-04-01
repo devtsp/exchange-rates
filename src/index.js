448163060ed6ff181a258f69d82279c5e3e550b9
Tomas <87679143+devtsp@users.noreply.github.com>
2022-04-01 20:33:43 -0300

    change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 
@@ -11,7 +11,7 @@ ReactDOM.render(
 		<Provider store={store}>
 			<Router>
 				<Routes>
-					<Route path="exchange-rates/*" element={<App />}></Route>
+					<Route path="/*" element={<App />}></Route>
 				</Routes>
 			</Router>
 		</Provider>
