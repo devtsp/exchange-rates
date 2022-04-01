20c01bb770c371469f32435b08a1b89d28e92f2e
Tomas <87679143+devtsp@users.noreply.github.com>
2022-04-01 18:50:04 -0300

    move router and store provider to index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import { Provider } from 'react-redux';
-import { store } from './Redux/store';
 import Layout from './Layout';
 import RatesPanel from './components/RatesPanel';
 import ConversionPanel from './components/ConversionPanel';
 
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
 	return (
 		<div id="app-container">
-			<Provider store={store}>
-				<Router>
-					<Routes>
-						<Route path="/" element={<Layout />}>
-							<Route index element={<RatesPanel />} />
-							<Route path="pair-conversion" element={<ConversionPanel />} />
-						</Route>
-						<Route path="*" element={<h1>Missing</h1>} />
-					</Routes>
-				</Router>
-			</Provider>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<RatesPanel />} />
+					<Route path="pair-conversion" element={<ConversionPanel />} />
+				</Route>
+				<Route path="*" element={<h1>Missing</h1>} />
+			</Routes>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<Router>
+				<Routes>
+					<Route path="/*" element={<App />}></Route>
+				</Routes>
+			</Router>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
