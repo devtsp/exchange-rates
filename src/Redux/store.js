85680b7aa955d6927816c6e936d9a1544529fa50
Tomas <87679143+devtsp@users.noreply.github.com>
2022-06-14 11:29:10 -0300

    fix redux devtools causing bug on load

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -6,6 +6,8 @@ export const store = createStore(
 	reducer,
 	compose(
 		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		window.__REDUX_DEVTOOLS_EXTENSION__
+			? window.__REDUX_DEVTOOLS_EXTENSION__()
+			: f => f
 	)
 );
